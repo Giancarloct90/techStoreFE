96f4ebb6a487ca0f646fa0228b4fc1dd987dd7ce
GianJS <giancarloct90@hotmail.com>
2021-08-15 21:32:42 -0600

    15.08.21-0932pm

--- js/newProductsJS.js
+++ js/newProductsJS.js
@@ -5,10 +5,28 @@ const precio = document.getElementById('precio');
 const cantidad = document.getElementById('cantidad');
 const file = document.getElementById('file')
 const btnNuevoProducto = document.getElementById('btnNuevoProducto');
+const btnTest = document.getElementById('btnTest');
+const divNotify = document.getElementById('divNotify');
 // store page
 const navLinkStore = document.getElementById("navLinkStore");
 const divBurger = document.getElementById("divBurger");
 const divProductsCards = document.getElementById('divProductsCards');
+
+    const notify = (flag) => {
+        nombre.value = '';
+        precio.value = '';
+        cantidad.value = '';
+        file.value = '';
+        flag ? divNotify.style.backgroundColor = 'rgb(90, 196, 86)' : divNotify.style.backgroundColor = 'rgb(209, 90, 90)';
+        divNotify.classList.toggle('enableNotify');
+        setTimeout(() => {
+            divNotify.removeAttribute("style");
+            divNotify.classList.toggle('enableNotify');
+            divNotify.style.animation = 'fadeOutNotify 0.35s ease-in-out';
+        }, 2500);
+
+
+    }
 
 // BURGER BOTTON FUNCTION
 divBurger.addEventListener('click', () => {
@@ -20,4 +38,5 @@ divBurger.addEventListener('click', () => {
 btnNuevoProducto.addEventListener('click', async (e) => {
     let res = await dataFunctions.saveProduct(nombre.value.trim(), precio.value.trim(), cantidad.value.trim(), file.files[0]);
     console.log(res);
+    res.ok ? notify(true) : notify(false);
 })
--- js/store.js
+++ js/store.js
@@ -4,6 +4,15 @@ const dataFunctions = new DataFunctions();
 const navLinkStore = document.getElementById("navLinkStore");
 const divBurger = document.getElementById("divBurger");
 const divProductsCards = document.getElementById('divProductsCards');
+const loadDiv = document.getElementById('loadDiv');
+// const btnTest = document.getElementById('btnTest');
+
+// btnTest.addEventListener('click', () => {
+//     loadDiv.style.animation = 'load 3s ease';
+//     setTimeout(() => {
+//         loadDiv.style.opacity = '0';
+//     }, 3000);
+// });
 
 // BURGER BOTTON FUNCTION
 divBurger.addEventListener('click', () => {
@@ -13,6 +22,7 @@ divBurger.addEventListener('click', () => {
 });
 
 const renderProdcuts = async () => {
+    loadDiv.style.opacity = '1';
     let products = await dataFunctions.getAllProducts();
     let html = '';
     if (products.ok) {
@@ -31,11 +41,20 @@ const renderProdcuts = async () => {
             }
             html += `<button>ver detalle</button>`;
             html += `</div>`;
+
+            // LOADING GIF FADE OUT
+            loadDiv.style.animation = 'load 0.8s ease';
+            setTimeout(() => {
+                loadDiv.style.opacity = '0';
+            }, 800);
+
+
             const div = document.createElement('div');
             div.classList.add('divCards');
             div.innerHTML = html;
             divProductsCards.appendChild(div);
             html = '';
+
         });
     } else {
         console.log('error server');
